d426528e470dbeb6a632bfea451a938c26596a6a
Cyril Chandelier <cyril.chandelier@gmail.com>
2015-11-04 15:11:48 +0100

    Add unit tests
    Credits goes to Matt Malone's blog post (http://oldfashionedsoftware.com/2009/11/19/string-distance-and-refactoring-in-scala/) where the tests were found before being translated to this Swift implementation

--- Sample/LevenshteinTests/LevenshteinTests.swift
+++ Sample/LevenshteinTests/LevenshteinTests.swift
@@ -9,28 +9,61 @@
 import UIKit
 import XCTest
 
-class LevenshteinTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+/**
+ These tests were found in the following blog post and translated to this Swift implementation :
+ http://oldfashionedsoftware.com/2009/11/19/string-distance-and-refactoring-in-scala/
+ */
+class LevenshteinTests: XCTestCase
+{
+    func testThatDistanceWorksOnEmptyStrings()
+    {
+        XCTAssertEqual("".levenshtein(""), 0)
+        XCTAssertEqual("".levenshtein("a"), 1)
+        XCTAssertEqual("".levenshtein("abc"), 3)
+        XCTAssertEqual("a".levenshtein(""), 1)
+        XCTAssertEqual("abc".levenshtein(""), 3)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testThatDistanceWorksOnEqualStrings()
+    {
+        XCTAssertEqual("".levenshtein(""), 0)
+        XCTAssertEqual("a".levenshtein("a"), 0)
+        XCTAssertEqual("abc".levenshtein("abc"), 0)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testThatDistanceWorksWhereOnlyInsertsAreNeeded()
+    {
+        XCTAssertEqual("".levenshtein("a"), 1)
+        XCTAssertEqual("a".levenshtein("ab"), 1)
+        XCTAssertEqual("b".levenshtein("ab"), 1)
+        XCTAssertEqual("ac".levenshtein("abc"), 1)
+        XCTAssertEqual("abcdefg".levenshtein("xaxbxcxdxexfxgx"), 8)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testThatDistanceWorksWhereOnlyDeletesAreNeeded()
+    {
+        XCTAssertEqual("a".levenshtein(""), 1)
+        XCTAssertEqual("ab".levenshtein("a"), 1)
+        XCTAssertEqual("ab".levenshtein("b"), 1)
+        XCTAssertEqual("abc".levenshtein("ac"), 1)
+        XCTAssertEqual("xaxbxcxdxexfxgx".levenshtein("abcdefg"), 8)
     }
     
+    func testThatDistanceWorksWhereOnlySubsistutionsAreNeeded()
+    {
+        XCTAssertEqual("a".levenshtein("b"), 1)
+        XCTAssertEqual("ab".levenshtein("ac"), 1)
+        XCTAssertEqual("ac".levenshtein("bc"), 1)
+        XCTAssertEqual("abc".levenshtein("axc"), 1)
+        XCTAssertEqual("xaxbxcxdxexfxgx".levenshtein("1a2b3c4d5e6f7g8"), 8)
+    }
+    
+    func testThatDistanceWorksWhereManyOperationsAreNeeded()
+    {
+        XCTAssertEqual("example".levenshtein("samples"), 3)
+        XCTAssertEqual("sturgeon".levenshtein("urgently"), 6)
+        XCTAssertEqual("levenshtein".levenshtein("frankenstein"), 6)
+        XCTAssertEqual("distance".levenshtein("difference"), 5)
+        XCTAssertEqual("objc was neat".levenshtein("swift is great"), 9)
+    }
 }
